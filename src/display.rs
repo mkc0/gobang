d8db0283510558837a649e0cd27879fa14e98d76
chaos <re0pen@protonmail.com>
2021-10-28 01:03:10 +0800

    Display Feature: Add coord

--- src/display.rs
+++ src/display.rs
@@ -3,7 +3,13 @@ use crate::board::{Board, Chess};
 pub fn display(board: &Board) {
     let n = board.size();
     let coord = board.last_coord();
+    print!("  ");
     for i in 0..n {
+        print!("{:>2}", i as i32 - (n / 2) as i32);
+    }
+    println!();
+    for i in 0..n {
+        print!("{:>2}", i as i32 - (n / 2) as i32);
         for j in 0..n {
             if coord == Some((i, j)) {
                 print!(">");
