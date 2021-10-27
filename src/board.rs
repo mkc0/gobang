44f05113af6da861f90654f56c08a15120721719
chaos <re0pen@protonmail.com>
2021-10-28 00:56:31 +0800

    Add turn op

--- src/board.rs
+++ src/board.rs
@@ -19,13 +19,13 @@ impl Board {
         Board {
             n: n,
             board: (0..n).map(|_i| vec![0u8; n]).collect(),
-            turn: 1u8,
+            turn: 0,
             history: Vec::<(usize, usize)>::new(),
         }
     }
 
-    pub fn get_chess(self: &Self, i: usize, j: usize) -> Chess {
-        match self.board[i][j] {
+    fn turn_to_chess(x: u8) -> Chess {
+        match x {
             0u8 => return Chess::EMPTY,
             1u8 => return Chess::BLACK,
             2u8 => return Chess::WHITE,
@@ -33,16 +33,41 @@ impl Board {
         }
     }
 
+    fn valid_coord(self: &Self, i: usize, j: usize) -> bool {
+        i < self.n && j < self.n
+    }
+
+    pub fn next_turn(self: &Self) -> u8 {
+        if self.turn == 0u8 {
+            return 1u8;
+        }
+        3 - self.turn
+    }
+
+    pub fn prev_turn(self: &Self) -> u8 {
+        if self.turn == 0u8 || self.history.len() == 0 {
+            return 0u8;
+        }
+        3 - self.turn
+    }
+
+    pub fn get_chess(self: &Self, i: usize, j: usize) -> Chess {
+        if !self.valid_coord(i, j) {
+            return Chess::INVALID;
+        }
+        Board::turn_to_chess(self.board[i][j])
+    }
+
     // ret: Success or not
     pub fn play(self: &mut Self, i: usize, j: usize) -> bool {
-        if i >= self.n || j >= self.n {
+        if !self.valid_coord(i, j) {
             return false;
         }
         if self.board[i][j] != 0 {
             return false;
         }
+        self.turn = self.next_turn();
         self.board[i][j] = self.turn;
-        self.turn = 3 - self.turn;
         self.history.push((i, j));
         true
     }
@@ -54,7 +79,7 @@ impl Board {
         for _ in 0..nsteps {
             let (r, c) = self.history.pop().unwrap();
             self.board[r][c] = 0;
-            self.turn = 3 - self.turn;
+            self.turn = self.prev_turn();
         }
         true
     }
@@ -67,15 +92,21 @@ impl Board {
         Some(self.history[nhis - 1])
     }
 
+    // pub fn win(self: &Self) -> bool {
+    //     let turn = self.turn;
+    //     if turn == 0 {
+    //         return false;
+    //     }
+    //     let coord = self.last_coord().unwrap();
+        
+    //     true
+    // }
+
     pub fn size(self: &Self) -> usize {
         self.n
     }
 
     pub fn current_turn(self: &Self) -> Chess {
-        match self.turn {
-            1u8 => return Chess::BLACK,
-            2u8 => return Chess::WHITE,
-            _ => return Chess::INVALID,
-        }
+        Board::turn_to_chess(self.next_turn())
     }
 }
--- src/display.rs
+++ src/display.rs
@@ -2,10 +2,10 @@ use crate::board::{Board, Chess};
 
 pub fn display(board: &Board) {
     let n = board.size();
-    let last_coord = board.last_coord();
+    let coord = board.last_coord();
     for i in 0..n {
         for j in 0..n {
-            if last_coord == Some((i, j)) {
+            if coord == Some((i, j)) {
                 print!(">");
             } else {
                 print!(" ");
